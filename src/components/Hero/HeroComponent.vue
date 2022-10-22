f9c9ea351c1656646e467dba0095fbdb0ee9eac5
zuzexx <tjasa.zilavec@gmail.com>
2022-10-22 19:53:53 +0200

    .basic styling small mobile

--- src/components/Hero/HeroComponent.vue
+++ src/components/Hero/HeroComponent.vue
@@ -3,11 +3,11 @@
     class="bg-image bg-primary w-full mt-12 text-highligth font-semibold"
   >
     <div
-      class="flex flex-col justify-center items-center mx-auto text-center p-3"
+      class="flex flex-col justify-center items-center mx-auto text-center p-3 space-y-3"
     >
       <img class="mx-auto w-24" src="@/assets/logo.png" alt="logo image" />
-      <h2>Pasta with a soul</h2>
-      <hr />
+      <h2 class="uppercase text-lg">Pasta with a soul</h2>
+      <hr class="w-4/6 h-1 bg-dull border border-solid border-dull" />
       <div role="paragraph">Takeaway | Delivery</div>
     </div>
   </section>
