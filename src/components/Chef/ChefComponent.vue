46ae085d8eaef727020e2505c563e412807470f7
zuzexx <tjasa.zilavec@gmail.com>
2022-10-30 09:41:57 +0100

    Chef and RestaurantComponent final

--- src/components/Chef/ChefComponent.vue
+++ src/components/Chef/ChefComponent.vue
@@ -1,26 +1,39 @@
 <template>
   <section
-    class="mt-10 flex flex-col justify-center items-center text-left w-5/6 mx-auto space-y-3"
+    class="mt-10 flex flex-col justify-center items-center text-left w-5/6 mx-auto space-y-3 tablet:space-y-5 laptop:flex-row laptop:space-x-4 laptopl:space-x-5 laptopl:mt-18 4kay:space-x-8 4kay:mt-24"
   >
-    <h2 class="capitalize text-left text-3xl text-dark font-semibold pt-8">
-      chef with a capital letter
-    </h2>
-    <hr class="w-1/6 h-1 border border-solid border-secondary bg-secondary" />
-    <h3 class="uppercase text-left text-lg text-primary font-medium">
-      who is the master behind our food
-    </h3>
-    <div role="paragraph">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga velit, eaque
-      iste sint saepe voluptates pariatur ut distinctio esse ratione ea? Nam
-      laborum quod labore. Commodi et sunt provident in.
-    </div>
-    <div role="paragraph">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit,
-      eveniet nemo consequatur odit neque incidunt ut cum explicabo natus
-      blanditiis quisquam voluptatum voluptas quis facilis distinctio voluptatem
-      rem! Consequuntur, ab!
-    </div>
-    <img src="@/assets/chef.jpg" alt="chef image" />
+    <section
+      class="flex flex-col laptop:w-1/2 tablet:space-y-2 laptop:space-y-3 4kay:space-y-5"
+    >
+      <h2
+        class="capitalize text-left text-3xl tablet:text-4xl laptop:text-3xl laptopl:text-4xl 4kay:text-5xl text-dark font-semibold pt-8 laptop:pt-0"
+      >
+        chef with a capital letter
+      </h2>
+      <hr class="w-1/6 h-1 border border-solid border-secondary bg-secondary" />
+      <h3
+        class="uppercase text-left text-lg tablet:text-xl laptop:text-lg laptopl:text-xl 4kay:text-2xl text-primary font-medium"
+      >
+        who is the master behind our food
+      </h3>
+      <div role="paragraph">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga velit,
+        eaque iste sint saepe voluptates pariatur ut distinctio esse ratione ea?
+        Nam laborum quod labore. Commodi et sunt provident in.
+      </div>
+      <div role="paragraph">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit,
+        eveniet nemo consequatur odit neque incidunt ut cum explicabo natus
+        blanditiis quisquam voluptatum voluptas quis facilis distinctio
+        voluptatem rem! Consequuntur, ab!
+      </div>
+    </section>
+
+    <img
+      src="@/assets/chef.jpg"
+      alt="chef image"
+      class="laptop:w-1/2 4kay:w-1/3"
+    />
   </section>
 </template>
 
@@ -30,4 +43,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+div {
+  @apply text-base tablet:text-lg laptop:text-base laptopl:text-lg 4kay:text-xl;
+}
+</style>
--- src/components/Restaurant/RestaurantComponent.vue
+++ src/components/Restaurant/RestaurantComponent.vue
@@ -1,27 +1,39 @@
 <template>
   <section
     id="about-us"
-    class="mt-16 flex flex-col justify-center items-center text-left w-5/6 mx-auto space-y-3"
+    class="mt-16 flex flex-col justify-center items-center text-left w-5/6 mx-auto space-y-3 tablet:space-y-5 laptop:flex-row laptop:space-x-4 laptopl:space-x-5 laptopl:mt-18 4kay:space-x-8 4kay:mt-24"
   >
-    <img src="@/assets/restaurant.jpg" alt="restaurant image" />
-    <h2 class="capitalize text-left text-3xl text-dark font-semibold pt-8">
-      old story, new location
-    </h2>
-    <hr class="w-1/6 h-1 border border-solid border-secondary bg-secondary" />
-    <h3 class="uppercase text-left text-lg text-primary font-medium">
-      welcome to the pasta pot
-    </h3>
-    <div role="paragraph">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga velit, eaque
-      iste sint saepe voluptates pariatur ut distinctio esse ratione ea? Nam
-      laborum quod labore. Commodi et sunt provident in.
-    </div>
-    <div role="paragraph">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit,
-      eveniet nemo consequatur odit neque incidunt ut cum explicabo natus
-      blanditiis quisquam voluptatum voluptas quis facilis distinctio voluptatem
-      rem! Consequuntur, ab!
-    </div>
+    <img
+      src="@/assets/restaurant.jpg"
+      alt="restaurant image"
+      class="laptop:w-1/2 4kay:w-1/3"
+    />
+    <section
+      class="flex flex-col tablet:space-y-2 laptop:w-1/2 laptop:space-y-3 4kay:space-y-5"
+    >
+      <h2
+        class="capitalize text-left text-3xl tablet:text-4xl laptop:text-3xl laptopl:text-4xl 4kay:text-5xl text-dark font-semibold pt-8 laptop:pt-0"
+      >
+        old story, new location
+      </h2>
+      <hr class="w-1/6 h-1 border border-solid border-secondary bg-secondary" />
+      <h3
+        class="uppercase text-left text-lg tablet:text-xl laptop:text-lg laptopl:text-xl 4kay:text-2xl text-primary font-medium"
+      >
+        welcome to the pasta pot
+      </h3>
+      <div role="paragraph">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga velit,
+        eaque iste sint saepe voluptates pariatur ut distinctio esse ratione ea?
+        Nam laborum quod labore. Commodi et sunt provident in.
+      </div>
+      <div role="paragraph">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit,
+        eveniet nemo consequatur odit neque incidunt ut cum explicabo natus
+        blanditiis quisquam voluptatum voluptas quis facilis distinctio
+        voluptatem rem! Consequuntur, ab!
+      </div>
+    </section>
   </section>
 </template>
 
@@ -31,4 +43,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+div {
+  @apply text-base tablet:text-lg laptop:text-base laptopl:text-lg 4kay:text-xl;
+}
+</style>
